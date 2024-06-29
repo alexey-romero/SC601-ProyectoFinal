307945f8f05f4a06eaba9ab5a68076a305ba14bb
Alexey Romero <alexey.rs@outlook.com>
2024-06-29 12:55:50 -0600

    initial commit - project config files, gitignore

---
+++ SC601-ProyectoFinal/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SC601_ProyectoFinal.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
