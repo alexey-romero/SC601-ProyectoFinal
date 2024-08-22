717743c86b0b0d1110d2ebcc5c099c05d795cb63
Alexey Romero <alexey.rs@outlook.com>
2024-08-22 15:44:56 -0600

    refac again fuck COLOMBIA

--- SC601-ProyectoFinal/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace SC601_ProyectoFinal.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
