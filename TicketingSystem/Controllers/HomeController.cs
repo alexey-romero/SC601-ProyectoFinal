f8ea0c9654b95df81a0dc3bd6896387c78b9d5ae
RACHMAX <rialch14set@gmail.com>
2024-08-23 17:33:54 -0600

    feat: Add CreateRequest page initial frontEnd
    Bootstrap styled
    Initial createrequest.cshtml page for the frontend.
    Adjusted some styles

--- TicketingSystem/Controllers/HomeController.cs
+++ TicketingSystem/Controllers/HomeController.cs
@@ -23,6 +23,11 @@ namespace TicketingSystem.Controllers
             return View();
         }
 
+        public IActionResult CreateRequest()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
