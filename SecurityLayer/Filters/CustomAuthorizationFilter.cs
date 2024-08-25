66603b1e93ebfb12e56466e8699e5ce4b0efeba6
Alexey Romero <alexey.rs@outlook.com>
2024-08-25 13:16:46 -0600

    feat: security layer

---
+++ SecurityLayer/Filters/CustomAuthorizationFilter.cs
@@ -0,0 +1,19 @@
+﻿using TicketingSystem.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System.Security.Claims;
+
+namespace SecurityLayer.Filters;
+
+public class CustomAuthorizationFilter : IAuthorizationFilter
+{
+    public void OnAuthorization(AuthorizationFilterContext context) // Se ejecuta cada vez que se requiere autorización para una acción o controlador.
+    {
+        var user = context.HttpContext.User;
+        if (!user.Identity.IsAuthenticated)
+        {
+            // Verify if the user is logged in
+            context.Result = new RedirectToActionResult("Index", "Login", null);
+        }
+    }
+}
---
+++ SecurityLayer/SecurityService.cs
@@ -0,0 +1,38 @@
+﻿using RepositoryLayer;
+using RepositoryLayer.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecurityLayer;
+
+public interface ISecurityService
+{
+    Task<bool> AuthUserAsync(User user);
+    Task<bool> AuthUserByEmailAsync(User user);
+    Task<bool> RegisterUserAsync(User user);
+
+}
+
+public class SecurityService(ISecurityRepository securityRepository) : ISecurityService
+{
+    private readonly ISecurityRepository _securityRepository = securityRepository;
+
+    public async Task<bool> AuthUserAsync(User user)
+    {
+        return await _securityRepository.AuthenticateUserAsync(user);
+    }
+    public async Task<bool> AuthUserByEmailAsync(User user)
+    {
+        return await _securityRepository.AuthenticateUserAsync(user);
+    }
+
+    public async Task<bool> RegisterUserAsync(User user)
+    {
+        return await _securityRepository.RegisterUserAsync(user);
+    }
+
+}
