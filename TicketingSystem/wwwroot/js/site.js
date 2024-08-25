94358c7888720ac244ca9e7157049bf0bdda4d18
RACHMAX <rialch14set@gmail.com>
2024-08-25 15:44:45 -0600

    feat: Make Clear Fields button work with JS code
    CreateRequests Clear Fields button to work using site.js code with an events listener.

--- TicketingSystem/wwwroot/js/site.js
+++ TicketingSystem/wwwroot/js/site.js
@@ -2,3 +2,26 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+// Event Listener to Clear the Fields elements of the CreateRequest.cshtml form.
+
+document.addEventListener("DOMContentLoaded", function () {
+    const clearButton = document.querySelector("button.btn-secondary");
+
+    clearButton.addEventListener("click", function () {
+        // Clear the select elements
+        document.getElementById("RequestTypeControlSelect").selectedIndex = 0;
+        document.getElementById("RequestStatusControlSelect").selectedIndex = 0;
+
+        // Clear the text inputs
+        document.getElementById("titleField").value = "";
+        document.getElementById("subjectManager").value = "";
+        document.getElementById("ticketDescription").value = "";
+        document.getElementById("dueDate").value = "";
+        document.getElementById("attachmentFiles").value = "";
+        document.getElementById("revokingDate").value = "";
+        document.getElementById("adminNotes").value = "";
+        document.getElementById("resolutionInformation").value = "";
+    });
+});
+
